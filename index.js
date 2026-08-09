4ef5e2fd8fb2f0a7d0c9bbd73b4affeae394985e
akukamil <chamilioner_kid@mail.ru>
2026-08-09 07:58:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -890,7 +890,7 @@ game = {
 	},
 	
 	cache_updated(uid,player){		
-		for (const pcard of Objects.pCards){			
+		for (const pcard of objects.pCards){			
 			if (pcard.uid===uid){
 				pcard.tName.text=player.name
 				pcard.tRating.text=player.rating
